26f7af7a5e9c4230767facf22cbbc874e394688d
Mauri Vinicius Santos Moura <mauri.santos@cmtecnologia.com.br>
2019-07-02 16:24:43 -0300

    JAVA-SPRING

---
+++ clinicLala-api/src/main/java/com/example/cliniclala/api/ClinicLalaApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.cliniclala.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicLalaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicLalaApiApplication.class, args);
+	}
+
+}

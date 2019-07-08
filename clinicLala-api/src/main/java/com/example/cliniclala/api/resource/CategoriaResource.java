af8aac3b454843eea7109b9f7e3260c1078f30c6
Mauri Vinicius Santos Moura <mauri.santos@cmtecnologia.com.br>
2019-07-08 14:25:03 -0300

    Implementação API, Get & Post Categoria

--- clinicLala-api/src/main/java/com/example/cliniclala/api/resource/CategoriaResource.java
+++ clinicLala-api/src/main/java/com/example/cliniclala/api/resource/CategoriaResource.java
@@ -1,13 +1,23 @@
 package com.example.cliniclala.api.resource;
 
+import java.net.URI;
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
 import com.example.cliniclala.api.model.Categoria;
 import com.example.cliniclala.api.repository.CategoriaRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 /**
  * @author Vinic
@@ -17,14 +27,31 @@ import java.util.List;
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaResource {
-	
+
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
+
 	@GetMapping
-	public List<Categoria> Listar(){
+	public List<Categoria> Listar() {
 		return categoriaRepository.findAll();
-		
+
+	}
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response) {
+		Categoria categoriaSalva = categoriaRepository.save(categoria);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{cod}")
+				.buildAndExpand(categoriaSalva.getCod()).toUri();
+		response.setHeader("Location", uri.toASCIIString());
+
+		return ResponseEntity.created(uri).body(categoriaSalva);
+
+	}
+
+	@GetMapping("/cod")
+	public Categoria buscarPeloCodigo(Long cod) {
+		return categoriaRepository.findOne(cod);
 	}
 
 }

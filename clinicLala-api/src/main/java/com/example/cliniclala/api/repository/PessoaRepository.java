f1f7dc49b92c77e788dc428f5021f1c42ee5aa97
Mauri Vinicius <viniciusmauri@outlook.com.br>
2019-08-04 10:35:26 -0300

    Updates

--- clinicLala-api/src/main/java/com/example/cliniclala/api/repository/PessoaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.cliniclala.api.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.cliniclala.api.model.Pessoa;
-
-public interface PessoaRepository extends JpaRepository<Pessoa, Long>{
-
-}

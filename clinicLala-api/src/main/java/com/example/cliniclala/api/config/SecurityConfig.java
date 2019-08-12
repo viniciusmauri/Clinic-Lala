97ccdf08a952c6d19857d5b7795dd67a552af094
Mauri Vinicius Santos Moura <mauri.santos@cmtecnologia.com.br>
2019-08-12 17:44:03 -0300

    Atualização na Segurança de API

--- clinicLala-api/src/main/java/com/example/cliniclala/api/config/SecurityConfig.java
+++ clinicLala-api/src/main/java/com/example/cliniclala/api/config/SecurityConfig.java
@@ -1,11 +1,15 @@
 package com.example.cliniclala.api.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -15,6 +19,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication()
 		.withUser("admin").password("admin").roles("ROLE");
+	}
+	
+	@Bean
+	public UserDetailsService userDetailsService() {
+		User.UserBuilder builder = User.withDefaultPasswordEncoder();
+		InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+		manager.createUser(builder.username("admin").password("admin").roles("ROLE").build());
+		return manager;
 	}
 
 	@Override
